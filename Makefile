9dbf625ac16a86411407f58fc8db3b977d46838c
Jerome FELLUS <jfellus@gmail.com>
2018-09-17 16:51:48 +0200

    basic line plot ok

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 install:
 	mkdir -p /usr/lib/phantom-plot
-	cp ./* /usr/lib/phantom-plot
+	cp ./*.js ./phantom-plot phantomjs /usr/lib/phantom-plot
 	ln -s /usr/lib/phantom-plot/phantom-plot /usr/bin/phantom-plot
--- phantom-plot.js
+++ phantom-plot.js
@@ -4,42 +4,91 @@ var system = require('system');
 var fs = require('fs')
 var page = require('webpage').create();
 
+page.onConsoleMessage = function(msg) { console.log(msg); }
 page.content = '<html><body></body></html>';
-page.injectJs('./d3.v3.min.js');
+page.injectJs('./d3.v4.min.js');
 
-page.onConsoleMessage = function(msg) {
-  console.log(msg);
-}
+var PATH = fs.workingDirectory;
 
 function phantom_plot(input) {
   return page.evaluate(function(input) {
-    var body = d3.select('body');
-    body.html("")
-    var svg = body.append('svg');
-    svg.append('text').text(input);
-    svg.append('circle');
-    var s = new XMLSerializer();
-    return s.serializeToString(document.querySelector('svg'))
+    var data = input.data;
+    var margin = input.margin || {top: 20, right: 20, bottom: 30, left: 50}
+    var size = input.size || {w:960, h:500}
+
+    width = size.w - margin.left - margin.right
+    height = size.h - margin.top - margin.bottom
+
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+    if(input.css) svg.append("style").text("\n" + input.css + "\n")
+
+    svg = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d[0]; }));
+    y.domain([0, d3.max(data, function(d) { return d[1]; })]);
+
+    // Curves
+    function plot(data, colx, coly) {
+      colx = colx || 0
+      coly = coly || 1
+      var valueline = d3.line()
+      .x(function(d) { return x(d[colx]); })
+      .y(function(d) { return y(d[coly]); });
+
+      svg.append("path")
+      .data([data])
+      .style("fill", "none")
+      .style("stroke", "green")
+      .style("stroke-width", "5px")
+      .attr("d", valueline);
+    }
+
+    for(var i=1; i<data[0].length; i++) {
+      plot(data, 0, i);
+    }
+
+    // Axes
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+    svg.append("g")
+    .call(d3.axisLeft(y));
+
+
+    return new XMLSerializer().serializeToString(document.querySelector('svg'))
   }, input);
 }
 
+var input = {};
+input.csv = fs.read("./data.csv")
+input.data = input.csv.split("\n").filter(function(x){ return x.indexOf(",") !== -1;}).map(function(x) { return x.split(",").map(function(x) { return parseFloat(x); })});
+input.data.shift()
+
 if(system.args[1] === '-h') {
   console.log(
-" phantom-plot : an easy-to-write plotter that outputs SVG using D3 and PhantomJS\n"    +
-" --------\n" +
-" USAGE\n" +
-" --------\n" +
-"    phantom-plot -h : Display this help\n" +
-"\n" +
-"    phantom-plot: Wait for input statements and\n" +
-"        output SVG to stdout\n"+
-"\n" +
-"    phantom-plot <inline-statements> : Process the\n"+
-"        inline-statements and output SVG to stdout\n" +
-"\n" +
-"    phantom-plot --multiple-files : Each input line\n"+
-"        must be \"infile outfile\". For each line, \n" +
-"        process the infile and write SVG to corresponding outfile\n"
+    " phantom-plot : an easy-to-write plotter that outputs SVG using D3 and PhantomJS\n"    +
+    " --------\n" +
+    " USAGE\n" +
+    " --------\n" +
+    "    phantom-plot -h : Display this help\n" +
+    "\n" +
+    "    phantom-plot: Wait for input statements and\n" +
+    "        output SVG to stdout\n"+
+    "\n" +
+    "    phantom-plot <inline-statements> : Process the\n"+
+    "        inline-statements and output SVG to stdout\n" +
+    "\n" +
+    "    phantom-plot --multiple-files : Each input line\n"+
+    "        must be \"infile outfile\". For each line, \n" +
+    "        process the infile and write SVG to corresponding outfile\n"
   )
 
 }
@@ -51,7 +100,8 @@ else if(system.args[1] === "--multiple-files") {
       var line = system.stdin.readLine();
       f = line.split(" ")
       if(f[0] && f[1]) {
-        out = phantom_plot(fs.read(f[0]));
+        input.x = fs.read(f[0]);
+        out = phantom_plot(input);
         fs.write(f[1], out, "w")
       }
     } catch(e) { console.error(e)}
@@ -59,11 +109,13 @@ else if(system.args[1] === "--multiple-files") {
 }
 
 else if(system.args.slice(1).join("")){
-  console.log(phantom_plot(system.args.slice(1).join("")))
+  input.x = system.args.slice(1).join("")
+  console.log(phantom_plot(input))
 }
 
 else {
-  console.log(phantom_plot(system.stdin.read()))
+  input.x = system.stdin.read()
+  console.log(phantom_plot(input))
 }
 
 phantom.exit();
